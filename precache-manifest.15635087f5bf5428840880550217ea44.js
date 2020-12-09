3d4df50d0b1d21d6afea846657e7bdbba4f91ea2
Dakyung Go <48652069+DakyungGo@users.noreply.github.com>
2020-12-09 22:51:23 +0900

    Update precache-manifest.15635087f5bf5428840880550217ea44.js
    add /mask-detector

--- precache-manifest.15635087f5bf5428840880550217ea44.js
+++ precache-manifest.15635087f5bf5428840880550217ea44.js
@@ -1,38 +1,38 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "daeb8a9f242da6abcfc3fe7f049029dd",
-    "url": "/index.html"
+    "url": "/mask-detector/index.html"
   },
   {
     "revision": "553ecc5a549528c11900",
-    "url": "/static/css/main.fd40bbe3.chunk.css"
+    "url": "/mask-detector/static/css/main.fd40bbe3.chunk.css"
   },
   {
     "revision": "12cd7f32f3c199ba9ffc",
-    "url": "/static/js/2.a2ed32d0.chunk.js"
+    "url": "/mask-detector/static/js/2.a2ed32d0.chunk.js"
   },
   {
     "revision": "6d80cc6046a43fcfb72f5495bf8999c0",
-    "url": "/static/js/2.a2ed32d0.chunk.js.LICENSE.txt"
+    "url": "/mask-detector/static/js/2.a2ed32d0.chunk.js.LICENSE.txt"
   },
   {
     "revision": "553ecc5a549528c11900",
-    "url": "/static/js/main.6843bcde.chunk.js"
+    "url": "/mask-detector/static/js/main.6843bcde.chunk.js"
   },
   {
     "revision": "780ee690d5944ac59e9c",
-    "url": "/static/js/runtime-main.b1e59c9a.js"
+    "url": "/mask-detector/static/js/runtime-main.b1e59c9a.js"
   },
   {
     "revision": "f6a9f98fe526d844b2d28b78eac19416",
-    "url": "/static/media/angry.f6a9f98f.png"
+    "url": "/mask-detector/static/media/angry.f6a9f98f.png"
   },
   {
     "revision": "52de6dd54c07cd2edbd0a65155b588ab",
-    "url": "/static/media/home.52de6dd5.png"
+    "url": "/mask-detector/static/media/home.52de6dd5.png"
   },
   {
     "revision": "dd9e8fe45a9ac5a66e8f5a43269093da",
-    "url": "/static/media/mask.dd9e8fe4.png"
+    "url": "/mask-detector/static/media/mask.dd9e8fe4.png"
   }
 ]);
