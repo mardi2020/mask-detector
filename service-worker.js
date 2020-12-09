8a480f5e09d36ceb7e6cae9965645d2aa47eddf5
Dakyung Go <48652069+DakyungGo@users.noreply.github.com>
2020-12-09 22:53:16 +0900

    Update service-worker.js
    add /mask-detector

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15635087f5bf5428840880550217ea44.js"
+  "/mask-detector/precache-manifest.15635087f5bf5428840880550217ea44.js"
 );
 
 self.addEventListener('message', (event) => {
